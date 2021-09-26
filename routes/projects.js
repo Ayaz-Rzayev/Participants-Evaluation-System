e3b900ac118a50c114687f48fe62a425367f3263
Ayaz-Rzayev <rzayev.ayaz198@gmail.com>
2021-09-26 17:51:18 +0400

    Update rates route added

--- routes/projects.js
+++ routes/projects.js
@@ -61,13 +61,13 @@ router.delete('/:id', isLogedIn, catchAsync(async (req, res, next) => {
 	res.redirect('/projects')
 }));
 
+// Rate
 router.get('/:id/rate', isLogedIn, catchAsync(async(req, res, next) => {
 	const {id} = req.params
   const currentUser = req.session.user_id
 	const project = await Project.findById(id).populate('participants').populate('pm')
 	res.render('projects/rate', {project, currentUser})
 }));
-
 
 router.post('/rate', isLogedIn, catchAsync(async(req, res, next) => {
   // insert into rateSchema
@@ -108,24 +108,66 @@ router.post('/rate', isLogedIn, catchAsync(async(req, res, next) => {
   res.redirect('/projects')
 }));
 
+// Edit Rates
 router.get('/:id/rate/edit', isLogedIn, catchAsync(async(req, res, next) => {
 	const {id} = req.params
   const currentUser = req.session.user_id
   const currentUserString = String(currentUser)
 	const project = await Project.findById(id).populate('participants').populate('pm')
   const rates = await Rates.find({project: id, voter: currentUserString}).populate('project').populate({path: 'project', populate:{path: 'pm'}}).populate('voter').populate('votes.participant').populate('votes')
-  // res.send(rate)
 	res.render('projects/editRate', {project, currentUser, rates})
 }));
+
+router.put('/:id/rate/edit', isLogedIn, catchAsync(async(req, res, next) => {
+  const {id} = req.params
+  const currentUser = req.session.user_id
+  const currentUserString = String(currentUser)
+  const{project, voter, participants, criteria1, criteria2, criteria3, criteria4, criteria5, criteria6, criteria7, criteria8,criteria9, criteria10, criteria11, criteria12} = req.body
+  for(let participant of participants){
+    await Rates.findOneAndDelete({"votes.participant": participant, project: id, voter: currentUserString})
+  }
+  for(let i= 0; i < participants.length; i++){
+    const criteriasPlain = [criteria1[i], criteria2[i], criteria3[i], criteria4[i], criteria5[i], criteria6[i], criteria7[i], criteria8[i], criteria9[i], criteria10[i]]
+    const criterias = []
+    for(let j = 0; j < criteriasPlain.length; j++){
+      if(criteriasPlain[j]){
+        criterias.push(parseInt(criteriasPlain[j]))
+      }
+    }
+    const average = criterias ? avg([avg(criterias), criteria11[i] ? parseInt(criteria11[i]) : 0 ]) : null
+    const rate = new Rates({
+      project,
+      voter,
+      pm,
+      votes:{
+        participant: participants[i],
+        criteria1: criteria1[i],
+        criteria2: criteria2[i],
+        criteria3: criteria3[i],
+        criteria4: criteria4[i],
+        criteria5: criteria5[i],
+        criteria6: criteria6[i],
+        criteria7: criteria7[i],
+        criteria8: criteria8[i],
+        criteria9: criteria9[i],
+        criteria10: criteria10[i],
+        criteria11: criteria11[i],
+        criteria12: criteria12[i],
+        average: average
+      }
+    })
+    await rate.save()
+  }
+  res.redirect('/projects')
+}))
 
 router.get('/:id/rates', isLogedIn, catchAsync(async(req, res, next) => {
   const {id} = req.params
   const rates = await Rates.find({project: id}).populate('project').populate({path: 'project', populate:{path: 'pm'}}).populate('voter').populate('votes.participant').populate('votes')
-  //res.send(rates)
-  // console.log(id)
   res.render('projects/rates', {rates, id})
 }));
 
+// Submit rates for average results
 router.post('/:id/rates', isLogedIn, catchAsync(async(req, res, next) => {
   const {id} = req.params
   const rates = await Rates.find({project: id}).populate('project').populate({path: 'project', populate:{path: 'pm'}}).populate('voter').populate('votes.participant').populate('votes')
@@ -165,10 +207,9 @@ router.post('/:id/rates', isLogedIn, catchAsync(async(req, res, next) => {
       averages = await AveragePoints.find({project: id})
     }
   }
-  //res.send(averages)
   res.redirect(`/projects/${id}/results`)
 }));
- 
+
 router.get('/:id/results', isLogedIn, catchAsync(async(req, res, next) => {
   const {id} = req.params
   const project = await Project.findById(id).populate('participants').populate('pm')
